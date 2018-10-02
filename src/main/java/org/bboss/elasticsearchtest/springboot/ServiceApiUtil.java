9c4c849464122f6acb6984ba0608066a71100e06
yinbp <yin-bp@163.com>
2018-10-02 09:33:31 +0800

    修改版本号为5.1.1 改进测试用例

--- src/main/java/org/bboss/elasticsearchtest/springboot/ServiceApiUtil.java
+++ src/main/java/org/bboss/elasticsearchtest/springboot/ServiceApiUtil.java
@@ -24,6 +24,7 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ServiceApiUtil {
+
 	private String mappath = "esmapper/demo.xml";
 	private ClientInterface restClient;
 	private ClientInterface restDemoConfigClient;
